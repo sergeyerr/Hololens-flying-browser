39c514caf7e7d1ceb151a5388ef657bcd87b43f1
sergeyerr <37268657+sergeyerr@users.noreply.github.com>
2018-08-05 16:04:33 +0500

    nicely done movement script

---
+++ browser/Assets/KeyBoardSummoner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.UI.Keyboard;
+public class KeyBoardSummoner : MonoBehaviour, IInputClickHandler
+{
+    UnityEngine.TouchScreenKeyboard keyboard;
+    public static string keyboardText = "";
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        keyboard = TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, false, false, false, false);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ browser/Assets/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    Transform CameraTrans;
+    Vector3 offset;
+    float idealLen;
+    bool move = false;
+    float speed = 10;
+	// Use this for initialization
+	void Start () {
+        var camera = Camera.main;
+        CameraTrans = camera.transform;
+        offset = transform.position - CameraTrans.position;
+        idealLen = offset.magnitude;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var browserToCamera = transform.position - CameraTrans.position;
+        var bRotation = Quaternion.LookRotation(browserToCamera);
+        var angleCamToB = Quaternion.Angle(CameraTrans.rotation, bRotation);
+        transform.position = CameraTrans.position + offset;
+        if (move)
+        {
+            Quaternion fullRotation = CameraTrans.rotation * Quaternion.Inverse(bRotation);
+            float scalingFactor = (speed + angleCamToB * angleCamToB / 100)  / angleCamToB * Time.smoothDeltaTime;
+            Quaternion scaledRotation = Quaternion.Slerp(Quaternion.identity, fullRotation, scalingFactor);
+            Vector3 newbrowserToCamera = scaledRotation * browserToCamera;
+            transform.position = CameraTrans.position + newbrowserToCamera.normalized*idealLen;
+            offset = newbrowserToCamera;
+            transform.rotation = Quaternion.LookRotation(-newbrowserToCamera) * Quaternion.Euler(90, 0, 0) * Quaternion.Euler(0, -180, 0);
+            angleCamToB = Quaternion.Angle(CameraTrans.rotation, bRotation);
+            if (angleCamToB < 7)
+            {
+                move = false;
+
+            }
+        }
+        if (angleCamToB > 25) move = true;
+    }
+}
